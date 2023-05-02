9f2a2b3e66363109e0977d1a9c457e5146921d2f
Victor S. Miller <victorsmiller@gmail.com>
2023-05-02 14:52:54 -0700

    Added big_or function

--- resolving/logic.py
+++ resolving/logic.py
@@ -1,9 +1,7 @@
 """
 Various utilities for generating CNF
 """
-from typing import Iterable, List, Tuple
-from itertools import product, chain
-from collections import Counter
+from typing import Iterable, List
 from pysat.formula import IDPool
 from pysat.card import CardEnc, EncType
 from .lex import lex_compare, Comparator
@@ -12,9 +10,32 @@ CLAUSE = List[int]
 FORMULA = List[CLAUSE]
 MODEL = List[int]
 
+def big_or(pool: IDPool,
+           formulas: List[FORMULA]) -> Iterable[CLAUSE]:
+    """
+    yield a CNF which is the OR of a list of CNF.
+    """
+    result = []
+    for form in formulas:
+        avatars = []
+        olit = pool._next()
+        result.append(olit)
+        for clause in form:
+            if len(clause) > 1:
+                lit = pool._next()
+                yield [-lit] + clause
+                yield from ([-elt, lit] for elt in clause)
+            else:
+                lit = clause[0]
+        avatars.append(lit)
+        # make olit <-> AND(avatars)
+        yield from ([-olit, _] for _ in avatars)
+        yield [olit] + [- _ for _ in avatars]
+    yield result
+    
 def implies(pool: IDPool,
-            form1: List[CLAUSE],
-            form2: List[CLAUSE]) -> Iterable[CLAUSE]:
+            form1: Iterable[CLAUSE],
+            form2: Iterable[CLAUSE]) -> Iterable[CLAUSE]:
     """
     Clauses instantiating cl1 -> cl2.
     """
@@ -24,12 +45,31 @@ def implies(pool: IDPool,
         if len(clause) > 1:
             lit = pool._next()
             yield [-lit] + clause
-            yield from ([lit, -elt] for elt in clause)
+            yield from ([-elt, lit] for elt in clause)
         else:
             lit = clause[0]
         avatars.append(- lit)
-    yield from (avatars + elt for elt in form2)
+    yield from (avatars + clause for clause in form2)
 
+def sum_not_zero(pool: IDPool, pos: List[int], neg: List[int],
+                 encode: str = 'totalizer') -> Iterable[CLAUSE]:
+    """
+    Clauses for a sum not = 0.
+
+    (S >= 0) ==> (S >= 1)
+    """
+    encoding = getattr(EncType, encode,
+                       EncType.totalizer)
+    gt0 = CardEnc.atleast(lits = pos + [- _ for _ in neg],
+                          bound = len(neg),
+                          encoding = encoding,
+                          vpool = pool).clauses
+    gt1 = CardEnc.atleast(lits = pos + [- _ for _ in neg],
+                          bound = len(neg) + 1,
+                          encoding = encoding,
+                          vpool = pool).clauses
+    yield from implies(pool, gt0, gt1)
+    
 def set_xor(lit: int, lit1: int, lit2:int) -> Iterable[CLAUSE]:
     """
     CNF for lit := (lit1 XOR lit2)
@@ -53,7 +93,7 @@ def set_and(lit: int, lit1: int, lit2: int) -> Iterable[CLAUSE]:
                 [-lit, lit2],
                 [lit, -lit1, -lit2])
 
-def negate(pool: IDPool, formula: FORMULA) -> Iterable[CLAUSE]:
+def negate(pool: IDPool, formula: Iterable[CLAUSE]) -> Iterable[CLAUSE]:
     """
     Negate a formula.
     """
@@ -76,5 +116,5 @@ def special_less(pool: IDPool,
                          encoding = getattr(EncType, encode,
                                             EncType.totalizer),
                          vpool = pool).clauses
-    lexlt = list(lex_compare(lit1, lit2, Comparator.LESS, pool = pool))
+    lexlt = list(lex_compare(pool, lit1, lit2, Comparator.LESS))
     yield from implies(pool, eql, eqc + lexlt)
