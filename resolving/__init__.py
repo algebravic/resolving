9943c694cc566129cafadbd698f3c55ec134c7fa
Victor S. Miller <victorsmiller@gmail.com>
2023-05-13 10:47:40 -0700

    Added ping_pong as globally known

--- resolving/__init__.py
+++ resolving/__init__.py
@@ -3,7 +3,9 @@ Calculate the metric dimension of the hypercube
 """
 from .resolve import resolve_hypercube_maxsat, resolve_hypercube_hitman
 from .resolve import resolve_hypercube_sat
+from .pingpong import ping_pong
 __all__ = ['resolve_hypercube_maxsat',
            'resolve_hypercube_sat',
-           'resolve_hypercube_hitman'
+           'resolve_hypercube_hitman',
+           'ping_pong'
            ]
