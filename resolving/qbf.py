97ec8311085350ee8c611c0c5d8de40b8a9017a4
Victor S. Miller <victorsmiller@gmail.com>
2023-05-15 18:15:04 -0700

    Made QBF the only thing in the file

--- resolving/qbf.py
+++ resolving/qbf.py
@@ -37,11 +37,8 @@ E[i,k, r + 1] = E[i,k,r] /\ (X[r,i] == X[r,k])
 """
 
 from typing import List, Tuple, Set, Iterable
-from pysat.formula import CNF, IDPool
-from pysat.card import CardEnc, EncType
 from itertools import chain
-from .lex import lex_compare
-from .check import resolvable_model
+from pysat.formula import CNF
 
 CLAUSE = List[int]
 
@@ -71,7 +68,7 @@ class QBF:
         """
         varset = _validate(variables)
         if varset is None:
-            raise ValueError("Variables must be a positive int")
+            raise ValueError(f"Variables must be a positive int {variables}")
         overlap = self._quantified.intersection(varset)
         if overlap:
             raise ValueError(f"Variables have already been quantified: {overlap}")
@@ -116,108 +113,6 @@ class QBF:
             fil.write(f"p cnf {len(self._model)} {nvars}\n")
             fil.write('\n'.join((f"{quant[0]} {' '.join(map(str, quant[1]))} 0")
                                   for quant in self._quantifiers))
-            fil.write('n')
-            fil.write('\n'.join(' '.join(map(str, _)) + '0') for _ in self._model)
-            fil.write('n')
-
-def parity(num: int, par: int) -> Iterable[Tuple[int, ...]]:
-    """
-    Generate all odd/even parity of length n.
-    """
-    if num == 0:
-        if par == 0:
-            yield tuple()
-        return
-    yield from ((0,) + _ for _ in parity(num - 1, par))
-    yield from ((1,) + _ for _ in parity(num - 1, 1 - par))
-
-def parity_constraint(lits: List[int], par: int) -> Iterable[List[int]]:
-    """
-    Parity constrained clauses.
-    """
-    num = len(lits)
-    # Forbid opposite parity elements
-    yield from ( ( (1-2 * _[0]) * _[1] for _ in zip(cons, lits))
-                 for cons in parity(num, 1 - par))
-
-def pre_post(pre: List[CLAUSE],
-             post: List[CLAUSE],
-             pool: IDPool) -> Iterable[CLAUSE]:
-    """
-    Generate clauses for pre ==> post
-    """
-    cvars = []
-    for clause in pre:
-        if len(clause) == 1:
-            cvars.append(- clause[0])
-        else:
-            new_var = pool._next()
-            cvars.append(- new_var)
-            yield [-new_var] + clause
-            yield from ([new_var, -_] for _ in clause)
-    yield from (cvars + _ for _ in post)
-
-def equiv(lft: List[CLAUSE],
-          rgt: List[CLAUSE],
-          pool: IDPool) -> Iterable[CLAUSE]:
-    """
-    Generate clauses for pre <==> post
-    """
-    yield from pre_post(lft, rgt, pool)
-    yield from pre_post(rgt, lft, pool)
-    
-def quantified_hypercube(num: int, bound: int) -> Tuple[QBF, IDPool]:
-    """
-    Create a QBF formula for checking if beta_n = bound.
-    """
-    qbf = QBF()
-    cnf = CNF()
-    pool = IDPool()
-
-    for ind in range(bound - 1):
-        for jind in range(num):
-            cnf.extend(parity_constraint([pool.id(('u', ind, jind)),
-                                          pool.id(('x', ind, jind)),
-                                          pool.id(('y', jind))], 1))
-            cnf.extend(parity_constraint([pool.id(('v', ind, jind)),
-                                          pool.id(('x', ind, jind)),
-                                          pool.id(('z', jind))], 1))
-    cnf.append([pool.id(('x', _)) for _ in range(num)] +
-               [pool.id(('y', _)) for _ in range(num)])
-
-    cnf.extend([[-pool.id(('y', _)), - pool.id(('z', _))]
-                for _ in range(num)])
-    for jind in range(num):
-        cnf.extend(CardEnc.equals(lits =
-                                  [pool.id(('u', jind, _))
-                                   for _ in range(num)] +
-                                  [- pool.id(('v', jind, _))
-                                   for _ in range(num)],
-                                  encoding = encoding,
-                                  bound = num,
-                                  vpool = pool))
-        
-    for ind in range(bound - 2):
-        cnf.extend(CardEnc.atleast(lits =
-                                   [pool.id(('x', ind+1, _))
-                                    for _ in range(num)] +
-                                   [-pool.id(('x', ind, _))
-                                    for _ in range(num)],
-                                   encoding = encoding,
-                                   bound = num,
-                                   vpool = pool))
-    for ind in range(bound - 2):
-        equ = CardEnc.equals(lits =
-                             [pool.id(('x', ind+1, _))
-                              for _ in range(num)] +
-                             [-pool.id(('x', ind, _))
-                              for _ in range(num)],
-                             encoding = encoding,
-                             bound = num,
-                             vpool = pool)
-
-        lexical = lex_less([pool.id(('x', ind, _)) for _ in range(num)],
-                           [pool.id(('x', ind + 1, _)) for _ in range(num)])
-
-        cnf.extend(pre_post(equ, lexical, pool))
-        
+            fil.write('\n')
+            fil.write('\n'.join((' '.join(map(str, _)) + ' 0') for _ in self._model))
+            fil.write('\n')
