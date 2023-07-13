a17e5f988e9b08a40ca1c9321afce7d24c025858
Victor S. Miller <victor.miller@sri.com>
2023-07-13 08:20:09 -0700

    Special cases for popcount for small bit widths

--- resolving/smt.py
+++ resolving/smt.py
@@ -5,12 +5,13 @@ from itertools import chain
 from pysmt.shortcuts import Symbol, ForAll, Exists, And, Or, Not, Int
 from pysmt.shortcuts import Equals, NotEquals, LE, GE, Plus
 from pysmt.typing import INT, BVType
+from pysmt.fnode import FNode
 
-def popcount(xexpr: BVType) -> INT:
+def popcount(xexpr: FNode) -> FNode:
     """
     This only works if width <= 32
 
-    From Hacker's Delight:
+    From Hacker's Delight (RIP Hank W.)
 
     int pop1(unsigned x) {
         x -= ((x >> 1) & 0x55555555);
@@ -21,24 +22,45 @@ def popcount(xexpr: BVType) -> INT:
         return x & 0x0000003F;
     }
     """
-    width = xexpr.bv_width()
+    try:
+        width = xexpr.bv_width()
+    except AssertionError as msg:
+        raise ValueError("Input is not a bitvector expression")
     if width > 32:
         raise ValueError("Only allow bitvecs of length <= 32")
-    mask = 2 ** width - 1
-
-    x1 = xexpr - ((xexpr >> 1) & (0x55555555 & mask))
-    x2 = ((x1 & (0x33333333 & mask))
-          + ((x1 >> 2) & (0x33333333 & mask)))
-    x3 = (x2 + (x2 >> 4)) & (0x0F0F0F0F & mask)
-
-    if width > 8:
+    mask = (1 << width) - 1
+    # Pocketed calculation
+    # First add, in parallel, every other bit
+    x1 = (xexpr & (0x55555555 & mask)
+          + ((xexpr >> 1) & (0x55555555 & mask)))
+    # x1 = xexpr - ((xexpr >> 1) & (0x55555555 & mask))
+    # Now 2 bit fields added in parallel
+    if width <= 2:
+        return x1 & 0x03
+        # x2 = (x1 & (0x33333333 & mask))
+    else:
+        x2 = ((x1 & (0x33333333 & mask))
+              + ((x1 >> 2) & (0x33333333 & mask)))
+    # Now 4 bit fields added in parallel
+    if width <= 4:
+        return x2 & 0x07
+        # x3 = x2 & (0x0F0F0F0F & mask)
+    else:
+        x3 = ((x2 & (0x0F0F0F0F & mask))
+              + ((x2 >> 4) & (0x0F0F0F0F & mask)))
+    # Now 8 bit fields added in parallel: max value fits in 5 bits
+    if width <= 8:
+        return x3 & 0x0F
+        # x4 = x3
+    else:
         x4 = x3 + (x3 >> 8)
+
+    if width <= 16:
+        return x4 & 0x1F
+        # x5 = x4
     else:
-        x4 = x3
-    if width > 16:
         x5 = x4 + (x4 >> 16)
-    else:
-        x5 = x4
+
     return x5 & (0x0000003F & mask)
 
 def smt_bv_model(num: int, mnum: int, reverse: bool = True):
@@ -46,26 +68,25 @@ def smt_bv_model(num: int, mnum: int, reverse: bool = True):
     Use bitvector for the model.
     """
     # The a variables
-    avars = [Symbol(f'a_{ind}', BVType(num))
+    avars = [Symbol(f'a_{num}_{ind}', BVType(num))
              for ind in range(mnum - 1)]
     # Since they are ordered, we only need say that the first is nonzero
     # However, putting in the explicit condition helps
-    a_not_empty = And(*(_.NotEquals(0) for _ in avars))
-    a_bound = And(*(popcount(_) <= num // 2 for _ in avars))
+    a_restrict = And(*(_.NotEquals(0) & (popcount(_) <= num // 2)
+                       for _ in avars))
     a_ord = And(*(_[0] < _[1] for _ in zip(avars[:-1], avars[1:])))
-    a_cond = a_not_empty & a_bound & a_ord
+    a_cond = a_restrict & a_ord
 
     # The two x variables, and their conditions
-    xvars = [Symbol(f'x_{_}', BVType(num)) for _ in range(2)]
+    xvars = [Symbol(f'x_{num}_{_}', BVType(num)) for _ in range(2)]
 
     disjoint_x = (xvars[0] & xvars[1]).Equals(0)
-    # This is implied by disjx but might be hard to deduce
-    bound_x = (popcount(xvars[0]) <= num // 2
-              & popcount(xvars[1]) <= num // 2)
-    x_not_empty = xvars[0].NotEquals(0) & xvars[1].NotEquals(0)
     equal_card = popcount(xvars[0]).Equals(popcount(xvars[1]))
+    # This is implied by disjx but might be hard to deduce
+    x_restrict = And(*((popcount(_) <= num // 2) & _.NotEquals(0)
+                       for _ in xvars))
     x_ord = xvars[0] > xvars[1] # A simple symmetry breaker
-    x_cond = disjoint_x & bound_x & x_not_empty & equal_card & x_ord
+    x_cond = disjoint_x & x_restrict & equal_card & x_ord
 
     # Linking of the x variables and a variables
     resolved = Or(*(
