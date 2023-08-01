413c946ab2a432028fce8a0878e8a3c251b50bd7
Victor S. Miller <victor.miller@sri.com>
2023-08-01 08:57:05 -0700

    Added explanatory description of public functions

--- resolving/__init__.py
+++ resolving/__init__.py
@@ -1,5 +1,16 @@
 """
 Calculate the metric dimension of the hypercube
+
+For general graphs the functions
+resolve_hypercube_maxsat, resolve_hypercube_sat and resolve_hypercube_hitman
+model the problem as a hitting set problem.
+
+For the special family of graphs, the hypercube, ping_pong
+models it as an 'exists/forall' problem which overcomes the
+exponential dependence on the parameter n.
+
+Finally, hypercube_model and inverse_hypercube_model
+construct a QDIMACS model as a QBF problem.
 """
 from .resolve import resolve_hypercube_maxsat, resolve_hypercube_hitman
 from .resolve import resolve_hypercube_sat
