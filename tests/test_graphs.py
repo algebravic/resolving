f1213e0f4ee6ed6f687a9e5920cde235eecf1d30
Victor S. Miller <victor.miller@sri.com>
2023-08-07 14:13:03 -0700

    Added tests for Johnson Graphs

--- tests/test_graphs.py
+++ tests/test_graphs.py
@@ -2,19 +2,12 @@
 
 import networkx as nx
 from pysat.formula import WCNF, IDPool
-from resolving.resolve import resolving_model, solve_maxsat, get_answer
+from resolving.resolve import resolve_maxsat
+from resolving.graphs import johnson_graph
 
 def graph(gph: nx.Graph, dim: int):
-    """Test if a graph has the indicated metric dimension
 
-    Args:
-    gph: A networkx Graph
-    dim: The asserted metric dimension
-    """
-    cnf = WCNF()
-    pool = resolving_model(gph, cnf)
-    soln = solve_maxsat(cnf)
-    result = get_answer(soln, pool) if soln is not None else None
+    result = resolve_maxsat(gph)
     assert (result is not None) and len(result) == dim
 
 def cycle(num: int):
@@ -42,6 +35,15 @@ def complete(num: int):
     """
     graph(nx.complete_graph(num), num - 1)
 
+def johnson(num: int, knum: int):
+
+    assert knum == 2
+    gph = johnson_graph(num, knum)
+    # Cf. Bailey et. al. Resolving sets for Johnson and Kneser graphs
+    small = {3: 2, 4: 3, 5: 3}
+    answer = small.get(num, (2 * (num - (num % 3))) // 3 + (num % 3))
+    graph(gph, answer)
+    
 class TestGraph:
 
     def test_5cycle(self):
@@ -58,3 +60,12 @@ class TestGraph:
  
     def test_k15(self):
         complete(15)
+
+    def test_j5(self):
+          johnson(5,2)
+
+    def test_j6(self):
+          johnson(6,2)
+
+    def test_j10(self):
+          johnson(10,2)
